16b92165e405b3e100bfd4bbbf4257321942ccd1
Arqiuomi <149426138+Arqiuomi@users.noreply.github.com>
2024-11-08 08:03:42 +0300

    materialdesin

---
+++ view/OpenWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+using Microsoft.Windows.Themes;
+
+namespace TrueSound.Properties
+{
+    /// <summary>
+    /// Логика взаимодействия для OpenWindow.xaml
+    /// </summary>
+    public partial class OpenWindow : Window
+    {
+        public OpenWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper _paletteHelper = new PaletteHelper();
+
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+    }
+}
