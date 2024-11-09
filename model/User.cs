46ddeb65174a086a92a4b0c1348b76ee230a68da
Arqiuomi <149426138+Arqiuomi@users.noreply.github.com>
2024-11-09 10:23:22 +0300

    add class User

---
+++ model/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TrueSound.model
+{
+    public class User : DependencyObject
+    {
+        public static readonly DependencyProperty NameProperty;
+        public static readonly DependencyProperty PasswordProperty;
+
+        static User()
+        {
+            NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(User));
+            PasswordProperty = DependencyProperty.Register("Password", typeof(string), typeof(User));
+        }
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+    }
+}
