9e7b2b450e1818c1bd1222372a65b31e4cce84ce
Arqiuomi <149426138+Arqiuomi@users.noreply.github.com>
2025-01-22 23:40:28 +0300

    add MyLikePage refactor PlayerPage

---
+++ View/MyLikePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TrueSound.ViewModel;
+
+namespace TrueSound.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MyLikePage.xaml
+    /// </summary>
+    public partial class MyLikePage : Page
+    {
+        public MyLikePage()
+        {
+            InitializeComponent();
+        }
+        public MyLikePage(MainViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -9,6 +9,7 @@ using TrueSound.view;
 using TrueSound.View;
 using TrueSound.model;
 using TrueSound.Model;
+using System.Windows.Media.Imaging;
 namespace TrueSound.ViewModel
 {
     public class MainViewModel: BaseViewModel
@@ -17,8 +18,10 @@ namespace TrueSound.ViewModel
         public DelegateCommand ProfileCommand { get; }
         public DelegateCommand LibraryCommand { get; }
         public DelegateCommand PlayerCommand { get; }
-        public DelegateCommand SettingCommand { get; }
+        public DelegateCommand MyLikeCommand { get; }
+        public DelegateCommand<object> LikePressCommand { get; }
 
+        public string CommandParameter { get; set; }
 
         public MainViewModel(OpenViewModel vm) 
         {
@@ -26,6 +29,9 @@ namespace TrueSound.ViewModel
             ProfileCommand = new DelegateCommand(OnProfileCommand);
             LibraryCommand = new DelegateCommand(OnLibraryCommand);
             PlayerCommand = new DelegateCommand(OnPlayerCommand);
+            MyLikeCommand = new DelegateCommand(OnMyLikeCommand);
+            LikePressCommand = new DelegateCommand<object>(OnLikePressCommand);
+            CommandParameter = "image/like.png";
         }
 
         private void OnProfileCommand()
@@ -40,16 +46,32 @@ namespace TrueSound.ViewModel
         {
             var pageSwitcher = (Frame)Application.Current.Windows[0].FindName("PageSwitcher");
             pageSwitcher.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
-            LibraryPage libraryPage = new LibraryPage(this); //передали тот самый объект vm из конструктора ; создать конструктор для vm юзера
+            LibraryPage libraryPage = new LibraryPage(this);
             pageSwitcher.Content = libraryPage;
         }
         private void OnPlayerCommand()
         {
             var pageSwitcher = (Frame)Application.Current.Windows[0].FindName("PageSwitcher");
             pageSwitcher.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
-            PlayerPage playerPage = new PlayerPage(this); //передали тот самый объект vm из конструктора ; создать конструктор для vm юзера
+            PlayerPage playerPage = new PlayerPage(this);
             pageSwitcher.Content = playerPage;
         }
+
+        private void OnMyLikeCommand()
+        {
+            var pageSwitcher = (Frame)Application.Current.Windows[0].FindName("PageSwitcher");
+            pageSwitcher.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+            MyLikePage myLikePage = new MyLikePage(this);
+            pageSwitcher.Content = myLikePage;
+        }
+
+        private void OnLikePressCommand(object parameter)
+        {
+            string imagePath = parameter as string;
+            CommandParameter = imagePath == "image/like.png" ? "image/filledLike.png" : "image/like.png";
+            
+        }
+
 
 
         public string Name
